198a28e43da844633bb42c2f98cb36e9df2bc539
takuya kinoshita <kktak02@gmail.com>
2022-05-14 06:36:10 +0900

    modified layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,26 +15,19 @@ class UsersController < ApplicationController
 
 
     @fixed_expense = current_user.fixed_expenses.find_by(pay_month: pay_month, pay_year: pay_year)
+    @variable_expense = current_user.variable_expenses.find_by(pay_month: pay_month, pay_year: pay_year)
     @hash = {}
     begin 
       FixedExpense::VALUES.each do |v|
         @hash[v] = @fixed_expense.send(v)
       end
+      VariableExpense::VALUES.each do |v|
+        @hash[v] = @variable_expense.send(v)
+      end      
       @hash.compact!
     rescue NoMethodError
       flash[:alert] = "データ未作成"
       redirect_to fixed_expense_index_path
-    end
-
-    @variable_expense = current_user.variable_expenses.find_by(pay_month: pay_month, pay_year: pay_year)
-    begin
-      VariableExpense::VALUES.each do |v|
-        @hash[v] = @variable_expense.send(v)
-      end
-      @hash.compact!
-    rescue
-      flash[:alert] = "データ未作成"
-      redirect_to variable_expense_index_path
     end
   end
 
